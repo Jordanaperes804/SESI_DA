4e5fa7c684ca42255e2fc0e50078384b6808674d
Jordana <jorperes53@gmail.com>
2026-05-20 11:25:18 -0300

    orçamento.decoração

--- projeto java/script.js
+++ projeto java/script.js
@@ -1,15 +1,18 @@
 function multiplicacao() {
-    var campo1 = document.getElementById("campo1")
-    var campo2 = document.getElementById("campo2")
-    var campo3 = document.getElementById("campo3")
-    var resultado = document.getElementById("resultado")
+    var nome = document.getElementById("nome");
+    var flor = document.getElementById("flor");
+    var mesa = document.getElementById("mesa");
+    var resultado = document.getElementById("resultado");
 
-    let mesa = 100.00
-     resultado.innerHTML = "resultado:" + (Number(campo3.value) * Number(mesa))
-  
+    let total_mesa = Number(mesa.value) * 95.23;
+    resultado.innerHTML = "resultado:" + ()
+    let total_flor = Number(flor.value) * 19.75;
+    
 
-
-
+   resultado.innerHTML =
+        nome + ", o valor é: " +total.toLocaleString("pt-BR")
+      });
+    
 }
 
 
