bf1745714a8482e9ca2cf54ba6741b2891e72c66
Jordana <jorperes53@gmail.com>
2026-05-21 11:32:04 -0300

    aula2105

--- projeto java/script.js
+++ projeto java/script.js
@@ -1,18 +1,19 @@
 function multiplicacao() {
-    var nome = document.getElementById("nome");
-    var flor = document.getElementById("flor");
-    var mesa = document.getElementById("mesa");
-    var resultado = document.getElementById("resultado");
+  var nome = document.getElementById("nome").value;
+  var flor = document.getElementById("flor").value;
+  var mesa = document.getElementById("mesa").value;
+  var resultado = document.getElementById("resultado");
 
-    let total_mesa = Number(mesa.value) * 95.23;
-    resultado.innerHTML = "resultado:" + ()
-    let total_flor = Number(flor.value) * 19.75;
-    
+  let total_mesa = Number(mesa) * 95.23;
+  // resultado.innerHTML = "resultado:" + ()
+  let total_flor = Number(flor) * 19.75;
+  let total = total_mesa + total_mesa;
 
-   resultado.innerHTML =
-        nome + ", o valor é: " +total.toLocaleString("pt-BR")
-      });
-    
+  resultado.innerHTML = "O valor é: R$ " + total.toLocaleString("pt-BR", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  });
+
 }
 
 
